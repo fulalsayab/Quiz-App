410dc123c4ce0d245581cee4dd0753a881c98bb9
falsaya <falsaya@ncsu.edu>
2021-03-09 15:02:17 -0500

    Adding correct and incorrect feedback to the chocies

--- game.js
+++ game.js
@@ -75,7 +75,14 @@ getNewQuestion = () => {
             acceptingAnswer = false;
             const selectedChoice = e.target;
             const selectedAnswer = selectedChoice.dataset['number'];
+            const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+            selectedChoice.parentElement.classList.add(classToApply);
+            setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
             getNewQuestion();
+
+            }, 1000)
+
         })
     })
 }
