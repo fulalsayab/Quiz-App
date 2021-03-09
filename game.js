a905eafe3fe1fff3684f26cf3de0795a8e0da6f7
falsaya <falsaya@ncsu.edu>
2021-03-09 13:13:39 -0500

    adding the start game funtinality and the new quastions funtinality

---
+++ game.js
@@ -0,0 +1,82 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+let currentQuestion = {};
+let acceptingAnswer = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+let questions = [
+    {
+        question:'Inside which HTML element do we put the JavaScript??',
+        choice1:'<script>',
+        choice2:'<javascript>',
+        choice3:'<js>',
+        choice4:'<scripting>',
+        answer: 1,
+        
+    },
+    {
+        question:"What is the correct syntax for referring to an external script called 'xxx.js'?'",
+        choice1:'<script>',
+        choice2:'<javascript>',
+        choice3:'<js>',
+        choice4:'<scripting>',
+        answer: 3,
+    },
+
+    {
+        question: " How do you write 'Hello World' in an alert box?",
+        choice1: "msgBox('Hello World');",
+        choice2: "alertBox('Hello World');",
+        choice3: "msg('Hello World');",
+        choice4: "alert('Hello World');",
+        answer: 4,
+    }
+];
+
+//CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUSETIONS = 3;
+
+//Start the Quiz
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+}
+
+//Gets the question for the quiz
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUSETIONS) {
+        //Go to the end page
+        return window.location.assign('/end.html');
+    }
+
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    //
+    choices.forEach((choice) => {
+        // const number = choice.dataset['number']
+        // choice.innerText = currentQuestion[`choice${number}`];
+        choice.innerText = currentQuestion[`choice${choice.dataset['number']}`];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswer = true;
+
+    choices.forEach((choice) => {
+        choice.addEventListener('click', e => {
+            if (!acceptingAnswer) return;
+
+            acceptingAnswer = false;
+            const selectedChoice = e.target;
+            const selectedAnswer = selectedChoice.dataset['number'];
+            getNewQuestion();
+        })
+    })
+}
+startGame();
