080dcef483224b3b1f72bb25e3e7fa17c72994f8
fulalsayab <ful.alsyab92@gmail.com>
2021-03-10 12:21:04 -0500

    Created a Spinning Loader

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progressBarFull");
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
 let currentQuestion = {};
 let acceptingAnswer = false;
 let score = 0;
@@ -36,6 +38,7 @@ fetch(
 
       return formattedQuestion;
     });
+
     startGame();
   })
   .catch((err) => {
@@ -53,6 +56,8 @@ startGame = () => {
   score = 0;
   availableQuestions = [...questions];
   getNewQuestion();
+  game.classList.remove("hidden");
+  loader.classList.add("hidden");
 };
 
 //Gets the question for the quiz
